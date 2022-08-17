8b9f88c0f5df5fcd0d5b140f3f2a1fa004719b5a
Oleksandra <ashchyhorieva@gmail.com>
2022-08-17 17:45:36 +0300

    task_01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
+import '../css/common.css'
+import '../css/01-gallery.css'
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+//console.log(galleryContainer);
+
+const galleryCardMarkup = createImageCardsMarkup(galleryItems);
+//console.log(galleryCardMarkup);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) =>  {
+    return `<div>
+<a class="gallery__item" href='${original}'>
+  <img 
+  class="gallery__image"
+  src='${preview}' 
+  alt='${description}'
+  />
+</a>
+  </div>`;
+  })
+    .join('');
+};
+
+const gallery = new SimpleLightbox('.gallery a', { 
+  captionSelector: 'img',
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom'
+});
