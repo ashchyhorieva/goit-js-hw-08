17455c4ab2dfa17ea92fba4f5c9536553e813f5c
Oleksandra <ashchyhorieva@gmail.com>
2022-08-17 23:13:03 +0300

    task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import * as throttle from 'lodash.throttle';
 
+console.log(throttle);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+//player.on('play', function() {
+//    console.log('played the video!');
+//});
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+console.log(LOCALSTORAGE_KEY);
+
+const onPlay = throttle(data => {
+    localStorage.setItem(LOCALSTORAGE_KEY, `${data.seconds}`);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+console.log(player.on);
+
+const currentTime = +localStorage.getItem(LOCALSTORAGE_KEY);
+
+console.log('currentTime', currentTime);
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
